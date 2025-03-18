b32b623cab20bfa917786a9ffd0d307302f6766c
Brian Cosgrove <bcosgrove@paypal.com>
2025-03-18 21:08:32 +0000

    Remove link to unused nvim dotfiles

--- activate.sh
+++ activate.sh
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 dotfiles_dir=$(cd "$(dirname "$0")"; pwd)
-mkdir -p "${HOME}/.config/nvim"
 
 if ! vim --version | grep -q '2nd user vimrc file'; then
   for name in vim vimrc vimrc.bundles; do
@@ -11,9 +10,5 @@ if ! vim --version | grep -q '2nd user vimrc file'; then
     ln -s "${dotfiles_dir}/${name}" "${HOME}/.${name}"
   done
 fi
-for name in config/nvim/init.vim; do
-  rm -rf "${HOME}/.${name}"
-  ln -s "${dotfiles_dir}/${name}" "${HOME}/.${name}"
-done
 
 vim +PlugInstall +PlugClean! +qall
